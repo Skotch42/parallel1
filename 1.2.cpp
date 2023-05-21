221dddb5ba57d970a655cec3d778f0e79640fe52
Комардин Влад <99380380+Skotch42@users.noreply.github.com>
2023-05-21 22:55:05 +0700

    Update 1.2.cpp

--- 1.2.cpp
+++ 1.2.cpp
@@ -22,12 +22,14 @@ int main(int argc, char** argv)
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-	if (rank == 0) {
+	if (rank == 0) 
+	{
 		srand(time(0));
 		message = rand() % 10;
 
 		MPI_Send(&message, 1, MPI_INT, getNext(rank, size), tag, MPI_COMM_WORLD);
 		std::cout << "Process 0 : sent " << message << '\n';
+		
 		MPI_Recv(&message, 1, MPI_INT, (size - 1), tag, MPI_COMM_WORLD, &status);
 		std::cout << "Process 0 : got " << message << '\n';
 	}
@@ -35,6 +37,7 @@ int main(int argc, char** argv)
 	{
 		MPI_Recv(&message, 1, MPI_INT, getPrevious(rank), tag, MPI_COMM_WORLD, &status);
 		std::cout << "Process " << rank << " : got " << message << ", sent " << message + rank << '\n';
+		
 		message = message + rank;
 		MPI_Send(&message, 1, MPI_INT, getNext(rank, size), tag, MPI_COMM_WORLD);
 	}
